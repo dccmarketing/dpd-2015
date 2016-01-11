cf4c0ae8413e865497a370b0f2b88cc7e0b3350a
Chris Wilcoxson <chris@slushman.com>
2016-01-11 10:58:10 -0600

    scriptnames- replacements.

--- inc/actions-and-filters.php
+++ inc/actions-and-filters.php
@@ -71,7 +71,7 @@ class dpd_2015_Actions_and_Filters {
 	 */
 	public function admin_scripts_and_styles() {
 
-		wp_enqueue_style( 'scriptname-admin', get_stylesheet_directory_uri() . '/admin.css' );
+		wp_enqueue_style( 'dpd-2015-admin', get_stylesheet_directory_uri() . '/admin.css' );
 
 	} // admin_scripts_and_styles()
 
@@ -83,8 +83,8 @@ class dpd_2015_Actions_and_Filters {
 	public function more_scripts_and_styles() {
 
 		wp_enqueue_style( 'dashicons' );
-		wp_enqueue_script( 'scriptname-search', get_template_directory_uri() . '/js/hidden-search.min.js', array(), '20150807', true );
-		// wp_enqueue_style( 'scriptname-fonts', $this->fonts_url(), array(), null );
+		wp_enqueue_script( 'dpd-2015-search', get_template_directory_uri() . '/js/hidden-search.min.js', array(), '20150807', true );
+		// wp_enqueue_style( 'dpd-2015-fonts', $this->fonts_url(), array(), null );
 
 	} // more_scripts_and_styles()
 
@@ -95,7 +95,7 @@ class dpd_2015_Actions_and_Filters {
 	 */
 	function login_scripts() {
 
-		wp_enqueue_style( 'scriptname-login', get_stylesheet_directory_uri() . '/login.css', 10, 2 );
+		wp_enqueue_style( 'dpd-2015-login', get_stylesheet_directory_uri() . '/login.css', 10, 2 );
 		wp_enqueue_script( 'enquire', '//cdnjs.cloudflare.com/ajax/libs/enquire.js/2.1.2/enquire.min.js', array(), '20150804', true );
 
 	} // login_scripts()
